2efe7f64222642478359fac24f39a75e396762fa
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2022-02-14 15:56:19 +0100

    comment code

--- galsim.c
+++ galsim.c
@@ -10,6 +10,7 @@ typedef struct {
 
 
 void runSimulation(int N, particle* particles, double delta_t, int nsteps, int useGraphics, char* argv[]) {
+  // define constants
   const double smoothing = 1e-3;
   double G = 100.0 / (double)N;
 
@@ -21,15 +22,19 @@ void runSimulation(int N, particle* particles, double delta_t, int nsteps, int u
     SetCAxes(0,1);
   }
 
+  // Run simulation for nsteps
   for (int step = 0; step < nsteps; step++) {
+    // Reset force accumulators to zero
     for (int i = 0; i < N; i++) {
       particles[i].f_x = 0;
       particles[i].f_y = 0;
     }
+    // Calculate the forces on all particles
     for (int i = 0; i < N; i++) {
       particle p_i = particles[i];
       for (int j = i+1; j < N; j++) {
         particle p_j = particles[j];
+        // Force calculations
         double diff_x = p_i.x - p_j.x;
         double diff_y = p_i.y - p_j.y;
         double denom = (sqrt(diff_x * diff_x + diff_y * diff_y) + smoothing);
@@ -42,11 +47,13 @@ void runSimulation(int N, particle* particles, double delta_t, int nsteps, int u
 
         particles[i].f_x += f_x;
         particles[i].f_y += f_y;
+        
         particles[j].f_x -= f_x;
         particles[j].f_y -= f_y;
       }
     }
 
+    // Perform Symplectic Euler step
     for (int i = 0; i < N; i++) {
       double acc_x = particles[i].f_x / particles[i].mass;
       double acc_y = particles[i].f_y / particles[i].mass;
@@ -56,6 +63,7 @@ void runSimulation(int N, particle* particles, double delta_t, int nsteps, int u
       particles[i].y += delta_t * particles[i].vel_y;
     }
 
+    // Show graphics
     if (useGraphics) {
       ClearScreen();
       for (int i = 0; i < N; i++) {
@@ -68,6 +76,7 @@ void runSimulation(int N, particle* particles, double delta_t, int nsteps, int u
 
   }
   
+  // Keep the last frame active until user hits q
   if(useGraphics) {
     printf("Done.");
     while(!CheckForQuit()) {
@@ -80,10 +89,11 @@ void runSimulation(int N, particle* particles, double delta_t, int nsteps, int u
 
 int main(int argc, char* argv[]) {
   if (argc != 6) {
-    printf("Exactly 5 parameters should be given!\n");
+    printf("Exactly 5 parameters should be given like this: ./galsim N filename nsteps delta_t graphics\n");
     return 1;
   }
 
+  // Read in the cmdline arguments
   int N, nsteps, useGraphics;
   char* filename;
   double delta_t;
@@ -92,30 +102,34 @@ int main(int argc, char* argv[]) {
   nsteps = atoi(argv[3]);
   delta_t = atof(argv[4]);
   useGraphics = atoi(argv[5]);
-  printf("N = %d, filename = %s, nsteps = %d, delta_t = %f, useGraphics = %d\n", N, filename, nsteps, delta_t, useGraphics);
 
+  // Allocate a list to store all particles
   particle* particles = (particle*) malloc(N*sizeof(particle));
 
+  // Open file and raise error if it doesn't exists 
   FILE* f = fopen(filename, "r"); 
   if (f == NULL) {
     printf("file %s doesn't exists!", filename);
     return 1;
   } 
+
+  // Read in all particles
   for (int i = 0; i < N; i++) {
     fread(&(particles[i].x), sizeof(double), 1, f);     
     fread(&(particles[i].y), sizeof(double), 1, f);     
     fread(&(particles[i].mass), sizeof(double), 1, f);     
     fread(&(particles[i].vel_x), sizeof(double), 1, f);     
     fread(&(particles[i].vel_y), sizeof(double), 1, f);     
-    fread(&(particles[i].brightness), sizeof(double), 1, f);     
-    printf("x, y, mass: %f, %f, %f\n", particles[i].x, particles[i].y, particles[i].mass);
+    fread(&(particles[i].brightness), sizeof(double), 1, f);
   }
   fclose(f);
 
   runSimulation(N, particles, delta_t, nsteps, useGraphics, argv);
 
+  // Open the result file 
   f = fopen("result.gal", "w");
   
+  // Write the list of particles to file
   for (int i = 0; i < N; i++) {
     fwrite(&(particles[i].x), sizeof(double), 1, f);     
     fwrite(&(particles[i].y), sizeof(double), 1, f);     
